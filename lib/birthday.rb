c4158eeb3c22dd339c957426908e75887c3dd4f3
Sav Younts <savyounts@gmail.com>
2018-08-04 21:34:21 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.collect do |kids_name, age|
+  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end 
 end
